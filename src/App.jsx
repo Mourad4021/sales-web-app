dd2117f6b8816136c0a152f04a837de1c7d537a5
root <root@CL-7BK63J3.cdbdx.biz>
2023-09-07 15:05:13 +0200

    feat: 🎸 add Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,11 @@
+import Counter from "./Counter";
+
 export default function App() {
-  return "Sales";
+  return (
+    <>
+      <Counter />
+      <Counter initialValue={12} step={5} />
+      <Counter initialValue={15} />
+    </>
+  );
 }
