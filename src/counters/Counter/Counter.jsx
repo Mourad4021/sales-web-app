b0e20bfe7348a5c4c4c33e5d2d77743c6c702095
Mourad Ben Hlayem <mourad.benhlayem@cdbdx.biz>
2023-09-08 16:58:40 +0200

    feat: 🎸 add mui on counter

--- src/counters/Counter/Counter.jsx
+++ src/counters/Counter/Counter.jsx
@@ -1,15 +1,23 @@
 import { number } from "prop-types";
 import { useCounter } from "../../hooks";
+import { Button, ButtonGroup, Card, Typography } from "@mui/material";
+import { CardContent } from "./styles";
 
 export default function Counter({ initialValue = 0, step = 1 }) {
   const [counter, { increment, decrement }] = useCounter(initialValue, step);
 
   return (
-    <>
-      <p>{counter}</p>
-      <button onClick={increment}>+{step}</button>
-      <button onClick={decrement}>-{step}</button>
-    </>
+    <Card sx={{ maxWidth: 200, margin: "2rem" }} elevation={5}>
+      <CardContent>
+        <Typography variant="h3" component="p">
+          {counter}
+        </Typography>
+        <ButtonGroup variant="outlined">
+          <Button onClick={increment}>+{step}</Button>
+          <Button onClick={decrement}>-{step}</Button>
+        </ButtonGroup>
+      </CardContent>
+    </Card>
   );
 }
 
