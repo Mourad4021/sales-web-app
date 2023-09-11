3c14b0d3f4e981e1bba4f512c46304690d3818d5
Mourad Ben Hlayem <mourad.benhlayem@cdbdx.biz>
2023-09-11 14:27:04 +0200

    feat: 🎸 add cssBaseline

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,13 @@
 import { ThemeProvider } from "@emotion/react";
 import { RouterProvider } from "react-router-dom";
-
+import { CssBaseline } from "@mui/material";
 import { theme } from "./theme";
-import { Router } from "./Router.jsx";
+import { Router } from "./Router";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <RouterProvider router={Router} />;
     </ThemeProvider>
   );
